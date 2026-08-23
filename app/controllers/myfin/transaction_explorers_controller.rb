201986ff196c40b4c9459cab2e68b4ace2af0a7c
Jeff-Willett <234232955+Jeff-Willett@users.noreply.github.com>
2026-08-23 08:38:30 -0400

    Render Transaction Explorer from report results

--- app/controllers/myfin/transaction_explorers_controller.rb
+++ app/controllers/myfin/transaction_explorers_controller.rb
@@ -3,16 +3,9 @@ module Myfin
     MAX_VISIBLE_ROWS = 250
 
     def show
-      @result = Myfin::TransactionExplorerQuery.call(
-        user: Current.user,
-        filters: filter_params
-      )
-      @rows = @result.rows
-      @visible_rows = @rows.first(MAX_VISIBLE_ROWS)
-      @rollup_by_type = @result.rollup_rows.group_by(&:type)
-      @filter_options = @result.filter_options
-      @selected_filters = selected_filters
-      @metrics = metrics
+      filters = Myfin::TransactionExplorer::Filters.from_params(filter_params)
+      @report = Myfin::TransactionExplorer::Report.call(user: Current.user, filters: filters)
+      @visible_rows = @report.rows.first(MAX_VISIBLE_ROWS)
       @breadcrumbs = [
         [ t("breadcrumbs.home"), root_path ],
         [ t("myfin.transaction_explorer.title"), nil ]
@@ -30,31 +23,6 @@ module Myfin
           wdg_categories: [],
           jpw_categories: []
         )
-      end
-
-      def selected_filters
-        {
-          entity_ids: selected_or_all(:entity_ids, @filter_options.entities.map(&:first)),
-          years: selected_or_all(:years, @filter_options.years),
-          months: selected_or_all(:months, @filter_options.months),
-          types: selected_or_all(:types, @filter_options.types),
-          wdg_categories: selected_or_all(:wdg_categories, @filter_options.wdg_categories),
-          jpw_categories: selected_or_all(:jpw_categories, @filter_options.jpw_categories)
-        }
-      end
-
-      def selected_or_all(key, available)
-        requested = Array(params[key]).compact_blank.map(&:to_s)
-        requested.presence || available.map(&:to_s)
-      end
-
-      def metrics
-        {
-          transactions: @rows.size,
-          expenses: @rows.select { |row| row.type == "Expense" }.sum(BigDecimal("0")) { |row| row.amount.abs },
-          income: @rows.select { |row| row.type == "Income" }.sum(BigDecimal("0"), &:amount),
-          transfer_net: @rows.select { |row| row.type == "Transfer" }.sum(BigDecimal("0"), &:amount)
-        }
       end
   end
 end
--- test/controllers/myfin/transaction_explorer_controller_test.rb
+++ test/controllers/myfin/transaction_explorer_controller_test.rb
@@ -50,6 +50,8 @@ class MyfinTransactionExplorerControllerTest < ActionDispatch::IntegrationTest
       end
     end
     assert_select "[data-testid='transaction-explorer-shared-set'][data-ledger-count='1'][data-rollup-count='1']"
+    assert_select "section", text: /Transactions\s+1\s+Expenses\s+\$120\.00\s+Income\s+\$0\.00\s+Transfer net\s+\$0\.00/
+    assert_select "section", text: /Expense\s+\$120\.00\s+Shopping\s+\$120\.00\s+Groceries\s+\$120\.00/
     assert_select "tr[data-entry-id='#{personal_entry.id}']", count: 1
     assert_select "tr[data-entry-id='#{gci_entry.id}']", count: 0
     assert_select "a[href='#{myfin_transaction_explorer_path}']", text: /Explorer/
