45c89d2fbd0fd6efc294fea101f82de8035dab4f
Jeff-Willett <234232955+Jeff-Willett@users.noreply.github.com>
2026-08-22 22:32:30 -0400

    Add test-only preview sign in

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ require "sidekiq/web"
 require "sidekiq/cron/web"
 
 Rails.application.routes.draw do
+  resource :preview_session, only: :create if Rails.env.test?
+
   namespace :myfin do
     resource :reporting_profile, only: :update
     resource :transaction_explorer, only: :show
